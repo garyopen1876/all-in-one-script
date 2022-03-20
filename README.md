1107909120dbc8b91535b56b150ffd4773a5e4b6
unknown <garyopen1876@gmail.com>
2022-03-20 22:55:28 +0800

    windows script update

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # all-in-one-script
 
 install basic software with scripts
+
+## linux
+use terminal cd to your install.sh path and enter:
+
+    bash install.sh
+
+## windows
+use windows administrator to open power shell and enter your ps1's absolute path:  
+
+(just example)
+
+    C:\Users\user\Desktop\scripts_test\all-in-one-script\windows\install.ps1
--- windows/install.ps1
+++ windows/install.ps1
@@ -1,20 +1,124 @@
 Write-Host "Congratulations! Script Successfully"
 
 # Set-ExecutionPolicy AllSigned
-Set-ExecutionPolicy Bypass -Scope Process
+Set-ExecutionPolicy Bypass -Scope Process -Force
 
-# Install  chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-# Chocolatey install
+Write-Host "&&&&&&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@&&@@&&&&&&&&&&&&&&&&&&&&&PYYJ#&&&&&&&&&@&&&&&&&&&&&&&&&&&&&&&&&"
+Write-Host "&&&&&&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@&&&#BGP5YYYY5PGB#BGGGGB#B5P5G&&&&&&&&&&@&&&&&@&&&&&&&&&&&&&&@&&"
+Write-Host "&&&&&&&&&&&&&@@@@@@@@@@@@@@@@@@@&@&&BGY?7!!!!!7?????????77777?JJ5G#&&&&&&&&&&@&&&&&&&&&&&&&&&&&&&&&&"
+Write-Host "&&&&&&&&&&&&&@@@@@@@@@@@@@@@@@@&&B5?~~^~!77???JY5Y5YJJJ???777?777??JYPB#&&&&&@@@@@@&&@&&&@&&&&&&&&&&"
+Write-Host "&&&&&&&&&&&&&@&@@@@@@@@@@@@&&#BPJ!~~~~!!!77?JJJY55YYJJ?J??J7??!777777?JJ5B&&&&&&&@&&@@@@@@@&&&&&&&&&"
+Write-Host "&&&&&#&&&&&&&@@@@@@@@@@@&&#PJ!~^^^^^:^~7777?JYY55YYJJJJJJJ?J?77?7?!!77????5G#&&@&&&&@@@@@@@@@@@@&&@@"
+Write-Host "&&&&&#&&&&&&&@@&@&&&&#BBP5!!!~~^:..:.::^!77?JY5YYYYYJJ???77?7!!7!7??7????????YPB#&@@&&@@@@@@@@@@&&&&"
+Write-Host "&&&&&&&#&&&&@@&&#GGPPP5Y!~~!!!~~::.::^^^!!!7?JYYYJJJJ?7~~~~~^^~~!!!7??????J?7???J5#&@&&@@@@@@@@@@&&&"
+Write-Host "&&&&&&&&&&&##GP5YYYY5J~^^~!!^::^^:^^:^^:^~~7????J77!!^~~~~~~~~~~777777???7?J?????7?YB&@@&@@@&@@&@&&&"
+Write-Host "&&&&&&&#GPYJJJJYYY5Y!::^~!~^^^:.~~~^^^^^^~!!??777!~~^^^^^!!~!777777?????77????Y5Y???5GB#&&&@&&&&&&&&"
+Write-Host "&&&&BPY?7?J??7????!:::::~!YB&@&B?!~~~~~~!!!!7!7!~!!!!^~~^!??7~~^^~~77?J?77!777?5PP55P55PPG#&&&&&&&&&"
+Write-Host "&&&#7!!!77!!~^^^^^:::.....~B&&@&GBY7~~!~!777777777!!777!!777~^~^^~!7???J??77777JGBGGGPPPPGGGB&&&&&&&"
+Write-Host "#&#BJJJJ?777~^^:::..    ....^~7!7JJ777????JJYYJJJ?77??JPG#B##G&&#BG55YYY??777!7?JGGBGGPGPGGGGGB&&&&&"
+Write-Host "&######BBGP!::...       .......:~!7???JY55Y555YYYJJJJYPGB####&@@@@@&#BJ?J?77!!7?7JPBGGGPPPGGGPPPG&&&"
+Write-Host "##&#&#####5~:.              ...:^~~!777??JJJYJYJJJYJY5GG5Y7!!7777!!^^~~!7777!7??777JGGGGGPPPGPPP5PB#"
+Write-Host "##&#####B5!:.               .:^!??????7?7!!77??J?JJ?JJYJ?!~^^~~^~~~~~~!!~~~~~!77!!!~7Y55PPGGPP55YY55"
+Write-Host "########P7:.             ..!G##&&&&&&&&#BP?~^^~!!7!7??J7~:.::::::^~~~!~^...:~!!!!!!~~7J55YJJJY5YY5YJ"
+Write-Host "#######G7:.             ..G@@@&&###&&&&&@&&BJ~:::~~!7JY?!^.....:...::::.    :^!777!!7?7?YY?77?7!7?JJ"
+Write-Host "######BJ:.            .. :&@@@@@&&&@@@@@@@@@&J^..^7?7?YJ7~::........::.     .:^!!7?7~!7JYY7~~7JYJY??"
+Write-Host "######P!:.           .^. .B#&@@@@@@@@@@@@@@@@P~::~7?7?JYYJ!:......:::.      ...:~!7!~!7?JP5YY5PB##BG"
+Write-Host "#####BY^.            .:...J&&@@@@@@@@@&&&&&#G!::^7???JJY557^^::...::..     .....:~!!7???YGGG&&&&&&&&"
+Write-Host "####BP7:.             .:..^5#@@@@@@@@@&&&#PY7~^~!7JJY555PPJ7?!^...:::..   .::....:~!77J?JG&&&@@@@@@@"
+Write-Host "####B5~.              .^:^77YP#&@@@@@&#BP5??!!77JJYYY55PGG55Y?~:..:^^..   .::....:^^!7?J5G#&@@@@@@@@"
+Write-Host "####G?^..              ^?JJJ55PG&#B##BGGG5Y?777?JY5PPGPGGGG##BY~:^^??:.. ........::^~!77YP#@@@@@@@@@"
+Write-Host "####B5?~..            .:J5YPGGGG&&GPGGGGB5P5YJYYPPGGGGGB#&@@&PJ~^:?5?^.....:.....^^^~7?J5GB&@@@@@@@@"
+Write-Host "#####BG?:.             .^!~J5GGB###&###B#BBBBBBBB###&&&@@@@#5?!~~7PY~....:::...:^^^^~!JY5GB&@@@@@@@@"
+Write-Host "#####&#G7.               :^^!7?YPGBBGG#B#B#&&&&@@@@@@@@&&#B5J?7?YPY?^::::^^:::^^^~^~77Y55PB&@@@@@@@@"
+Write-Host "#####&&&B?^.             .!~^~7!JYPP5YYYPY5GB#@@@@@&&&BBGP5YYYY5Y?!^^^^^~^^::::^^^^~7?JYYP#&@@@@@@@@"
+Write-Host "####&&&&@&P!.             .~~7~^7?5?YJJYY5PGB#&&&&#BGGGPPPPP5YJ!^:::~!!~^^^^^^::::~!!7JY5G#&&&@@@@@@"
+Write-Host "####&&&@@@&P~.             .^!!!~7J?J?Y5PPPGGBBBGGPPGGGGGP5J7!^^^^~!77!~::::::::^^~!77?5B#&&&&&@&@@@"
+Write-Host "####&&&@@@@&P^                :7Y55P555PGGBBBGPGGGGGGGGP5Y?!!!~!77?7!^:..::::.:^^~~?JY5PB&&&&&&&&&@@"
+Write-Host "####&&&@@@@@&J..               .!YPGGBBBBBGGGGGGGGBBGGPY?7!!!7!!!!!!^::...::^~!777J5PGB#&&&&&&&&&&@@"
+Write-Host "####&&&@@@@@&?:..               .:75PGBBBBGGGGGGGGGGPYJ7~~~~~~~!!~~^^:::.:^^^~!JY55G##&&&&&&&&&&&@@@"
+Write-Host "####&&@@@@@&B~.....      .       .:^!JY5PPPPPGGGPPP5YJ?!~~!^^~^!~~~^::::::^~!77J5GB#&&&&&&&&&&&&&@@@"
+Write-Host "###&&&@@@@@&G^....  .......     .  .^~!JYY5555555YYYJ777~!!~~!!7!^^^^::~!!7??YPPG##&&&@@&&&&&&&&@@@@"
+Write-Host "###&&&@@@@@&P^..     .......:......:^^~!77JJJJJYJJJJ7!!!~~~!7!7?7~!!!!!7?JJPGGB&#&&&&&&&@&&&&&&&@&@@"
+Write-Host "###&&&@@@@@&P^...      .:^~77~~~::^:^~::~!!~7777!77!!!!!!!777?7??77?YY5PGGG###&&&&&&&&&&@@&&&&&&&@@@"
+Write-Host "###&&@@@@@@&B~....      ..^~!7?JJ777!7~!!~^!7??J777777777777JYYY5PPPGBBBB##&&&#&####&&&&&&&&&&&&&@@@"
+Write-Host "###&&&@@@@@@#?....           ...:^~!7?YYYYJ5GPPPP5Y555Y5PPPPPGGGGGGGP5555PBBBGGBB#####&&&&&&&&&@&@@@"
+Write-Host "##&&&&@@@@@&&Y:.....                .:^^!!!7??7????JYYJJJJJYYYJJ??7!!7??JJ55P55PPGBGGBBB##&&&&&@@@@@"
+Write-Host "##&&&@@@@@&&&G^.....                         ....:::...::^~~~~~~~~~!!7JY55Y5YYYYY5P5Y555G#&&&&&@@@@@"
+Write-Host "##&&&@@@@@@@&P^..:...  ...                     ........:.:^^^~~!~!77?JYYYYJJJJJJ??J???JYG#&&&&&@@@@@"
+Write-Host "##&&@@@@@@@@&P:..........               ........::::::::^^^~~~~7??JJYYYJ????J?77!!!77J5G#&&&&&&@@@@@"
+Write-Host "##&&@@@@@@@@&B~.........            .. ......::::::...:::^~!!7????JYJYJ????7!~^^~~~~!JG##&&&&&&&@@@@"
+Write-Host "##&&@@@@@@@@&#7.........            ........:::::^::::^^^^~!7???J?JJ?77!!!~^:^^^^~!7YPGGB#####&&&@@@"
+Write-Host "#&&&@@@@@@@@@&J......               .......:::::^^^^^^~~~~!7?J?????7!~~~^^::^^^^~75PGGGGGBBB##&##&&@"
+Write-Host "#&&&@@@@@@@@@B~......                ........::::^^^^^^~~!7!777?77!~^^^:::::::^~7J5555PPGGBBB###BBGB"
 
-# chocolatey gui
-choco install -y chocolateygui
-# node.js
-choco install -y nodejs
-# python 3
-choco install -y python3
-# 7z
-choco install -y 7z.install
-# git
-choco install -y git 
+
+function install-dev-tools(){
+	# Install Chocolatey
+	Write-Host "Install Chocolatey"
+	[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
+	iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+
+	# Chocolatey gui
+	Write-Host "Install Chocolatey gui"
+	choco install -y chocolateygui
+
+	# node.js
+	Write-Host "Install node.js"
+	choco install -y nodejs
+
+	# python 3
+	Write-Host "Install python 3"
+	choco install -y python3
+
+	# 7z
+	Write-Host "Install 7z"
+	choco install -y 7zip.install
+
+	# git
+	Write-Host "Install git"
+	choco install -y git 
+	
+	# virtualbox
+	Write-Host "Install virtualbox"
+	choco install -y virtualbox
+	
+	# ngrok
+	Write-Host "Install ngrok"
+	choco install -y ngrok
+	
+	# docker
+	Write-Host "Install docker"
+	choco install -y docker-desktop
+	
+	# vscode
+	Write-Host "Install vscode"
+	choco install -y vscode
+	
+	# nodepad++
+	Write-Host "Install nodepad++"
+	choco install -y notepadplusplus
+	
+	# mysql
+	Write-Host "Install mysql"
+	choco install -y mysql
+}
+
+
+function install-basic-tools() {
+	# googlechrome
+	Write-Host "Install googlechrome"
+	choco install googlechrome
+	
+}
+
+
+function install-all() {
+	Write-Host "Install dev tools"
+	install-dev-tools
+	Write-Host "Install basic tools"
+	install-basic-tools
+}
+
+install-all
